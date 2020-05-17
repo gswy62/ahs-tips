213e26bdcdf9524b1fa538d76aa3116709210136
gswy62 <64339427+gswy62@users.noreply.github.com>
2020-05-17 14:59:09 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+let pagePic = document.querySelector('img');
+
+pagePic.onclick = function() {
+    let mySrc = pagePic.getAttribute('src');
+    if (mySrc === 'images/ahs-logo.png') {
+        pagePic.setAttribute('src', 'images/baby-fritz.jpg');
+    } else {
+        pagePic.setAttribute('src', 'images/ahs-logo.png');
+    }
+}
+
+
+
+document.querySelector('h1').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+/*document.querySelector('img').onclick = function() {
+    alert('Hi!');
+}*/
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name: ');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Ready to take dubs, ' + myName + '?';
+    }  
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Ready to take dubs, ' + storedName + '?';
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
